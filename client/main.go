4f32d62738a8765bb7aa5a31a1115ebe6ea9403c
Colton Hanson <colton.hanson@growthzone.com>
2019-06-13 16:21:36 -0500

    Changing our Server and adding an API. working off a new tutorial

--- client/main.go
+++ client/main.go
@@ -1,89 +1,61 @@
 package main
 
 import (
-	"fmt"
-	"grpc_sandbox/proto"
 	"log"
-	"net/http"
-	"strconv"
 
-	"github.com/gin-gonic/gin"
+	"grpc_sandbox/api"
+
+	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
+// Authentication holds the login/password
+type Authentication struct {
+	Login    string
+	Password string
+}
+
+// GetRequestMetadata gets the current request metadata
+func (a *Authentication) GetRequestMetadata(context.Context, ...string) (map[string]string, error) {
+	return map[string]string{
+		"login":    a.Login,
+		"password": a.Password,
+	}, nil
+}
+
+// RequireTransportSecurity indicates whether the credentials requires transport security
+func (a *Authentication) RequireTransportSecurity() bool {
+	return true
+}
+
 func main() {
-	conn, err := grpc.Dial("localhost:4040", grpc.WithInsecure())
+	var conn *grpc.ClientConn
+
+	// Create the client TLS credentials
+	creds, err := credentials.NewClientTLSFromFile("cert/server.crt", "")
 	if err != nil {
-		panic(err)
+		log.Fatalf("could not load tls cert: %s", err)
 	}
 
-	client := proto.NewAddServiceClient(conn)
-
-	g := gin.Default()
-	g.GET("/add/:a/:b", func(ctx *gin.Context) {
-		a, err := strconv.ParseUint(ctx.Param("a"), 10, 64)
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Parameter A"})
-			return
-		}
-
-		b, err := strconv.ParseUint(ctx.Param("b"), 10, 64)
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Parameter B"})
-			return
-		}
-
-		req := &proto.Request{A: int64(a), B: int64(b)}
-		if response, err := client.Add(ctx, req); err == nil {
-			ctx.JSON(http.StatusOK, gin.H{
-				"result": fmt.Sprint(response.Result),
-			})
-		} else {
-			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		}
-	})
-
-	g.GET("/mult/:a/:b", func(ctx *gin.Context) {
-		a, err := strconv.ParseUint(ctx.Param("a"), 10, 64)
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Parameter A"})
-			return
-		}
-		b, err := strconv.ParseUint(ctx.Param("b"), 10, 64)
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Parameter B"})
-			return
-		}
-		req := &proto.Request{A: int64(a), B: int64(b)}
-
-		if response, err := client.Multiply(ctx, req); err == nil {
-			ctx.JSON(http.StatusOK, gin.H{
-				"result": fmt.Sprint(response.Result),
-			})
-		} else {
-			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		}
-	})
-
-	g.GET("/api/v1/scientists/:a", func(ctx *gin.Context) {
-		a, err := strconv.ParseUint(ctx.Param("a"), 10, 64)
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Parameter A"})
-			return
-		}
-
-		req := &proto.RequestById{A: int64(a)}
-		if response, err := client.Scientists(ctx, req); err == nil {
-			ctx.JSON(http.StatusOK, gin.H{
-				"result": fmt.Sprint(response),
-			})
-		} else {
-			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		}
-	})
-
-	if err := g.Run(":8080"); err != nil {
-		log.Fatalf("Failed to run server: %v", err)
+	// Setup the login/pass
+	auth := Authentication{
+		Login:    "john",
+		Password: "doe",
 	}
 
+	// Initiate a connection with the server
+	conn, err = grpc.Dial("localhost:7777", grpc.WithTransportCredentials(creds), grpc.WithPerRPCCredentials(&auth))
+	if err != nil {
+		log.Fatalf("did not connect: %s", err)
+	}
+	defer conn.Close()
+
+	c := api.NewPingClient(conn)
+
+	response, err := c.SayHello(context.Background(), &api.PingMessage{Greeting: "foo"})
+	if err != nil {
+		log.Fatalf("error when calling SayHello: %s", err)
+	}
+	log.Printf("Response from server: %s", response.Greeting)
 }
--- server/main.go
+++ server/main.go
@@ -1,56 +1,159 @@
 package main
 
 import (
-	"context"
-	"grpc_sandbox/proto"
+	"fmt"
+	"log"
 	"net"
+	"net/http"
+	"strings"
+
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+
+	"golang.org/x/net/context"
+
+	"grpc_sandbox/api"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/metadata"
 )
 
-type server struct{}
+// private type for Context keys
+type contextKey int
 
-type Scientist struct {
-	Id        int64
-	FirstName string
-	LastName  string
+const (
+	clientIDKey contextKey = iota
+)
+
+func credMatcher(headerName string) (mdName string, ok bool) {
+	if headerName == "Login" || headerName == "Password" {
+		return headerName, true
+	}
+	return "", false
 }
 
-func main() {
-	listener, err := net.Listen("tcp", ":4040")
+// authenticateAgent check the client credentials
+func authenticateClient(ctx context.Context, s *api.Server) (string, error) {
+	if md, ok := metadata.FromIncomingContext(ctx); ok {
+		clientLogin := strings.Join(md["login"], "")
+		clientPassword := strings.Join(md["password"], "")
+
+		if clientLogin != "john" {
+			return "", fmt.Errorf("unknown user %s", clientLogin)
+		}
+		if clientPassword != "doe" {
+			return "", fmt.Errorf("bad password %s", clientPassword)
+		}
+
+		log.Printf("authenticated client: %s", clientLogin)
+
+		return "42", nil
+	}
+	return "", fmt.Errorf("missing credentials")
+}
+
+// unaryInterceptor call authenticateClient with current context
+func unaryInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	s, ok := info.Server.(*api.Server)
+	if !ok {
+		return nil, fmt.Errorf("unable to cast server")
+	}
+	clientID, err := authenticateClient(ctx, s)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	srv := grpc.NewServer()
-	proto.RegisterAddServiceServer(srv, &server{})
-	reflection.Register(srv)
+	ctx = context.WithValue(ctx, clientIDKey, clientID)
 
-	if e := srv.Serve(listener); e != nil {
-		panic(e)
+	return handler(ctx, req)
+}
+
+func startGRPCServer(address, certFile, keyFile string) error {
+	// create a listener on TCP port
+	lis, err := net.Listen("tcp", address)
+	if err != nil {
+		return fmt.Errorf("failed to listen: %v", err)
 	}
 
+	// create a server instance
+	s := api.Server{}
+
+	// Create the TLS credentials
+	creds, err := credentials.NewServerTLSFromFile(certFile, keyFile)
+	if err != nil {
+		return fmt.Errorf("could not load TLS keys: %s", err)
+	}
+
+	// Create an array of gRPC options with the credentials
+	opts := []grpc.ServerOption{grpc.Creds(creds),
+		grpc.UnaryInterceptor(unaryInterceptor)}
+
+	// create a gRPC server object
+	grpcServer := grpc.NewServer(opts...)
+
+	// attach the Ping service to the server
+	api.RegisterPingServer(grpcServer, &s)
+
+	// start the server
+	log.Printf("starting HTTP/2 gRPC server on %s", address)
+	if err := grpcServer.Serve(lis); err != nil {
+		return fmt.Errorf("failed to serve: %s", err)
+	}
+
+	return nil
 }
 
-func (s *server) Add(ctx context.Context, request *proto.Request) (*proto.Response, error) {
-	a, b := request.GetA(), request.GetB()
+func startRESTServer(address, grpcAddress, certFile string) error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
 
-	result := a + b
+	mux := runtime.NewServeMux(runtime.WithIncomingHeaderMatcher(credMatcher))
 
-	return &proto.Response{Result: result}, nil
+	creds, err := credentials.NewClientTLSFromFile(certFile, "")
+	if err != nil {
+		return fmt.Errorf("could not load TLS certificate: %s", err)
+	}
+
+	// Setup the client gRPC options
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(creds)}
+
+	// Register ping
+	err = api.RegisterPingHandlerFromEndpoint(ctx, mux, grpcAddress, opts)
+	if err != nil {
+		return fmt.Errorf("could not register service Ping: %s", err)
+	}
+
+	log.Printf("starting HTTP/1.1 REST server on %s", address)
+	http.ListenAndServe(address, mux)
+
+	return nil
 }
 
-func (s *server) Multiply(ctx context.Context, request *proto.Request) (*proto.Response, error) {
-	a, b := request.GetA(), request.GetB()
+// main start a gRPC server and waits for connection
+func main() {
+	grpcAddress := fmt.Sprintf("%s:%d", "localhost", 7777)
+	restAddress := fmt.Sprintf("%s:%d", "localhost", 7778)
+	certFile := "cert/server.crt"
+	keyFile := "cert/server.key"
 
-	result := a * b
+	// fire the gRPC server in a goroutine
+	go func() {
+		err := startGRPCServer(grpcAddress, certFile, keyFile)
+		if err != nil {
+			log.Fatalf("failed to start gRPC server: %s", err)
+		}
+	}()
 
-	return &proto.Response{Result: result}, nil
-}
+	// fire the REST server in a goroutine
+	go func() {
+		err := startRESTServer(restAddress, grpcAddress, certFile)
+		if err != nil {
+			log.Fatalf("failed to start gRPC server: %s", err)
+		}
+	}()
 
-func (s *server) Scientists(ctx context.Context, request *proto.RequestById) (*proto.Scientist, error) {
-	a := request.GetA()
-
-	return &proto.Scientist{Id: a, FirstName: "Brian", LastName: "Smith"}, nil
+	// infinite loop
+	log.Printf("Entering infinite loop")
+	select {}
 }
